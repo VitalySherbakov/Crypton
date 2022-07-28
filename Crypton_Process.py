07ed4a3eeee46d88bf763d95ff215c2887792d7c
Gendalf_Istari_2016 <palantir2018see@gmail.com>
2022-07-28 09:30:17 +0300

    ыффыа

--- Crypton_Process.py
+++ Crypton_Process.py
@@ -25,12 +25,12 @@ class Crypton(object):
 		"""Получить Словари"""
 		dict_crypt={
 		  "lol_miner": CryptonObj("-xzvf","lolMiner","lolminer", "1.52a","lolMiner_v1.52a_Lin64.tar.gz"),
-		  "b_miner": CryptonObj("-xf","bminer","bminer","1.52a","bminer-v16.4.11-2849b5c-amd64.tar.gz"),
-		  "srb_miner": CryptonObj("-xf","srbminer","srbminer","1.52a","SRBMiner-Multi-1-0-2-Linux.tar.gz"),
-		  "nb_miner": CryptonObj("-xzf","nbminer","nbminer","1.52a","NBMiner_42.2_Linux.tar.gz"),
-		  "teamred_miner": CryptonObj("-xzf","teamredminer","1.52a","teamredminer","teamredminer-v0.10.2-linux.tar.gz"),
+		  "b_miner": CryptonObj("-xf","bminer","bminer","1.52a","bminer-v16.4.11-2849b5c-amd64.tar.xz"),
+		  "srb_miner": CryptonObj("-xf","srbminer","srbminer","1.52a","SRBMiner-Multi-1-0-2-Linux.tar.xz"),
+		  "nb_miner": CryptonObj("-xzf","nbminer","nbminer","1.52a","NBMiner_42.2_Linux.tgz"),
+		  "teamred_miner": CryptonObj("-xzf","teamredminer","1.52a","teamredminer-v0.10.2-linux.tgz"),
 		  "t_rex_miner": CryptonObj("-xzvf","t-rex","t_rex","1.52a","t-rex-0.26.4-linux.tar.gz"),
-		  "g_miner": CryptonObj("-xf","miner","gminer","1.52a","gminer_3_03_linux64.tar.gz")
+		  "g_miner": CryptonObj("-xf","miner","gminer","1.52a","gminer_3_03_linux64.tar.xz")
 		}
 		return dict_crypt
 	def GetDictName(self):
