30850370d58c159d79563b13280f3133ee8a5cac
Gendalf_Istari_2016 <palantir2018see@gmail.com>
2022-07-28 09:39:57 +0300

    sdasdasf

--- Crypton_Process.py
+++ Crypton_Process.py
@@ -13,8 +13,11 @@ class CryptonObj(object):
 		super(CryptonObj, self).__init__()
 		self.Arhiv = f"{folder_path}/{folder}/{arhiv}"
 		self.Arhiv_Extract=f"{folder_path}/{folder}"
-		self.Path_Sh=f"{folder_path}/{folder}/{folder2}/{sh}"
 		self.Cmd=cmd
+		if folder2==None:
+			self.Path_Sh=f"{folder_path}/{folder}/{sh}"
+		else:
+			self.Path_Sh=f"{folder_path}/{folder}/{folder2}/{sh}"
 
 class Crypton(object):
 	"""docstring for Crypton"""
@@ -25,12 +28,12 @@ class Crypton(object):
 		"""Получить Словари"""
 		dict_crypt={
 		  "lol_miner": CryptonObj("-xzvf","lolMiner","lolminer", "1.52a","lolMiner_v1.52a_Lin64.tar.gz"),
-		  "b_miner": CryptonObj("-xf","bminer","bminer","1.52a","bminer-v16.4.11-2849b5c-amd64.tar.xz"),
-		  "srb_miner": CryptonObj("-xf","srbminer","srbminer","1.52a","SRBMiner-Multi-1-0-2-Linux.tar.xz"),
-		  "nb_miner": CryptonObj("-xzf","nbminer","nbminer","1.52a","NBMiner_42.2_Linux.tgz"),
-		  "teamred_miner": CryptonObj("-xzf","teamredminer","teamredminer","1.52a","teamredminer-v0.10.2-linux.tgz"),
-		  "t_rex_miner": CryptonObj("-xzvf","t-rex","t_rex","1.52a","t-rex-0.26.4-linux.tar.gz"),
-		  "g_miner": CryptonObj("-xf","miner","gminer","1.52a","gminer_3_03_linux64.tar.xz")
+		  "b_miner": CryptonObj("-xf","bminer","bminer","bminer-v16.4.11-2849b5c","bminer-v16.4.11-2849b5c-amd64.tar.xz"),
+		  "srb_miner": CryptonObj("-xf","srbminer","srbminer","SRBMiner-Multi-1-0-2","SRBMiner-Multi-1-0-2-Linux.tar.xz"),
+		  "nb_miner": CryptonObj("-xzf","nbminer","nbminer","NBMiner_Linux","NBMiner_42.2_Linux.tgz"),
+		  "teamred_miner": CryptonObj("-xzf","teamredminer","teamredminer","teamredminer-v0.10.2-linux","teamredminer-v0.10.2-linux.tgz"),
+		  "t_rex_miner": CryptonObj("-xzvf","t-rex","t_rex",None,"t-rex-0.26.4-linux.tar.gz"),
+		  "g_miner": CryptonObj("-xf","miner","gminer",None,"gminer_3_03_linux64.tar.xz")
 		}
 		return dict_crypt
 	def GetDictName(self):
