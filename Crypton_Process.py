cf6a3b0cbc8675f3af1c618917b981f846cf464f
Gendalf_Istari_2016 <palantir2018see@gmail.com>
2022-07-28 08:59:40 +0300

    fesdfdsf

--- Crypton_Process.py
+++ Crypton_Process.py
@@ -8,20 +8,12 @@ class CryptonObj(object):
 	Arhiv=""
 	Arhiv_Extract=""
 	Path_Sh=""
-	def __init__(self, sh: str, folder: str, arhiv: str, folder_path="/content/Crypton"):
+	def __init__(self, sh: str, folder: str, folder2: str, arhiv: str, folder_path="/content/Crypton"):
 		super(CryptonObj, self).__init__()
 		self.Arhiv = f"{folder_path}/{folder}/{arhiv}"
 		self.Arhiv_Extract=f"{folder_path}/{folder}"
-		self.Path_Sh=f"{folder_path}/{folder}/{sh}"
+		self.Path_Sh=f"{folder_path}/{folder}/{folder2}/{sh}"
 
-
-#cryptons=[]
-
-# g1=CryptonObj("lolminer","lolminer","lolMiner_v1.52a_Lin64.tar.gz")
-# print(f"A: {g1.Arhiv}")
-# print(f"AE: {g1.Arhiv_Extract}")
-# print(f"S: {g1.Path_Sh}")
-#cryptons.append(g1)
 
 class Crypton(object):
 	"""docstring for Crypton"""
@@ -31,13 +23,13 @@ class Crypton(object):
 	def GetDictCrypton(self):
 		"""Получить Словари"""
 		dict_crypt={
-		  "lol_miner": CryptonObj("lolMiner","lolminer","lolMiner_v1.52a_Lin64.tar.gz"),
-		  "b_miner": CryptonObj("bminer","bminer","bminer-v16.4.11-2849b5c-amd64.tar.gz"),
-		  "srb_miner": CryptonObj("srbminer","srbminer","SRBMiner-Multi-1-0-2-Linux.tar.gz"),
-		  "nb_miner": CryptonObj("nbminer","nbminer","NBMiner_42.2_Linux.tar.gz"),
-		  "teamred_miner": CryptonObj("teamredminer","teamredminer","teamredminer-v0.10.2-linux.tar.gz"),
-		  "t_rex_miner": CryptonObj("t-rex","t_rex","t-rex-0.26.4-linux.tar.gz"),
-		  "g_miner": CryptonObj("miner","gminer","gminer_3_03_linux64.tar.gz")
+		  "lol_miner": CryptonObj("lolMiner","lolminer", "1.52a","lolMiner_v1.52a_Lin64.tar.gz"),
+		  "b_miner": CryptonObj("bminer","bminer","1.52a","bminer-v16.4.11-2849b5c-amd64.tar.gz"),
+		  "srb_miner": CryptonObj("srbminer","srbminer","1.52a","SRBMiner-Multi-1-0-2-Linux.tar.gz"),
+		  "nb_miner": CryptonObj("nbminer","nbminer","1.52a","NBMiner_42.2_Linux.tar.gz"),
+		  "teamred_miner": CryptonObj("teamredminer","1.52a","teamredminer","teamredminer-v0.10.2-linux.tar.gz"),
+		  "t_rex_miner": CryptonObj("t-rex","t_rex","1.52a","t-rex-0.26.4-linux.tar.gz"),
+		  "g_miner": CryptonObj("miner","gminer","1.52a","gminer_3_03_linux64.tar.gz")
 		}
 		return dict_crypt
 	def GetExtractAll(self, dictes: {}):
@@ -47,7 +39,3 @@ class Crypton(object):
 			arhives.append(f'-xzvf "{dictes[li].Arhiv}" -C "{dictes[li].Arhiv_Extract}"')
 		return arhives
 
-g=Crypton()
-mass=g.GetDictCrypton()
-
-g.GetExtractAll(mass)
